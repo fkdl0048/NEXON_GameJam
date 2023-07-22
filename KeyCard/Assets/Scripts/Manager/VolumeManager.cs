62c9c38b10445ce3b4758da12e6dcb9d4beb6ced
fkdl0048 <fkdl000048@gmail.com>
2023-07-22 19:02:26 +0900

    Feat: Scene Flow

--- KeyCard/Assets/Scripts/Manager/VolumeManager.cs
+++ KeyCard/Assets/Scripts/Manager/VolumeManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering.PostProcessing;
 
 public class VolumeManager : Singleton<VolumeManager>
 {
@@ -23,14 +23,13 @@ public class VolumeManager : Singleton<VolumeManager>
     {
         _volume = GetComponent<Volume>();
 
-        _volume.profile.TryGet(out _Vignette);
+       // _volume.profile.TryGet(out _Vignette);
 
         //StartCoroutine(StartVigette());
     }
 
     private IEnumerator StartVigette()
     {
-
         while (true)
         {
             // 효과를 줄이는 단계
@@ -40,7 +39,7 @@ public class VolumeManager : Singleton<VolumeManager>
             {
                 float t = elapsedTime / pulsateDuration;
                 //float intensity = Mathf.Lerp();
-                _Vignette.intensity = new ClampedFloatParameter(maxIntensity, minIntensity, t);
+                //_Vignette.intensity = new ClampedFloatParameter(maxIntensity, minIntensity, t);
                 elapsedTime += Time.deltaTime;
                 yield return null;
             }
@@ -52,10 +51,23 @@ public class VolumeManager : Singleton<VolumeManager>
             {
                 float t = elapsedTime / pulsateDuration;
                 //float intensity = Mathf.Lerp(minIntensity, maxIntensity, t);
-                _Vignette.intensity = new ClampedFloatParameter(minIntensity, maxIntensity, t);
+                //_Vignette.intensity = new ClampedFloatParameter(minIntensity, maxIntensity, t);
                 elapsedTime += Time.deltaTime;
                 yield return null;
             }
         }
     }
+    
+    public void StartVignette()
+    {
+        if (isPulsating)
+        {
+            return;
+        }
+
+        //_Vignette.color = new ColorParameter(Color.red);
+
+    }
+    
+    
 }
--- KeyCard/Assets/Scripts/Quiz/ObjectOutline.cs
+++ KeyCard/Assets/Scripts/Quiz/ObjectOutline.cs
@@ -4,20 +4,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ObjectOutline : MonoBehaviour
+public class ObjectOutline
 {
     private Material material;
 
-    private void Start()
+    public ObjectOutline(Material material)
     {
-        material = GetComponent<Image>().material;
+        this.material = material;
         
         if (material == null)
         {
             Debug.LogError("Material is null");
         }
     }
-    
+
     public void ObjectOutlineOn()
     {
         material.SetFloat("_OutlineAlpha", 1);
@@ -26,5 +26,15 @@ public class ObjectOutline : MonoBehaviour
     public void ObjectOutlineOff()
     {
         material.SetFloat("_OutlineAlpha", 0);
+    }
+    
+    public void ObjectOutlineRed()
+    {
+        material.SetColor("_OutlineColor", new Color(0.69f, 0.23f, 0.28f));
+    }
+    
+    public void ObjectOutlineWhite()
+    {
+        material.SetColor("_OutlineColor", Color.white);
     }
 }
